de88cd9112be061cd01de7bc3bf18f9482373573
Ashwini277 <devarushiashwini@gmail.com>
2024-08-22 11:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+it("Testcase 1", () => {
+    cy.visit("http://localhost:3000/");
 
-const getSum = () => {
-//Add your code here
-  
-};
+    // Wait for the elements to ensure they are loaded
+    cy.get(".price").should("exist");
 
-getSumBtn.addEventListener("click", getSum);
+    // Iterate over each price element and modify the content
+    cy.get(".price").each(($a, ind) => {
+        const newPrices = [55, 75, 100, 130, 50]; // New prices to be typed
 
+        cy.wrap($a).invoke("attr", "contenteditable", "true").then(() => {
+            cy.wrap($a).clear().type(newPrices[ind], { force: true });
+        });
+    });
+
+    // Click the button to trigger the total calculation
+    cy.get("button").click();
+
+    // Assert the total price
+    cy.get("#ans").should("contain", 410);
+});
